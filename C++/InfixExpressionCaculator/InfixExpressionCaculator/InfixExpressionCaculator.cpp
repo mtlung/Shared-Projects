49a2fbf3bd7513c110d6dfa5244a7265ec308d56
Patrick Shim <patrick.shim@live.co.kr>
2015-08-11 15:36:27 +0800

    MASTER.01.000

---
+++ C++/InfixExpressionCaculator/InfixExpressionCaculator/InfixExpressionCaculator.cpp
@@ -0,0 +1,14 @@
+// InfixExpressionCaculator.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include<iostream>
+using std::cout;
+
+int main()
+{
+	cout << "hello world";
+    return 0;
+}
+
