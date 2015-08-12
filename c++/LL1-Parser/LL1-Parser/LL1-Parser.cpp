5c44ea0e94fee91816e8e81c16a6c4ff2020a94c
Patrick Shim <patrick.shim@live.co.kr>
2015-08-12 23:05:58 +0800

    MASTER.00.001

---
+++ c++/LL1-Parser/LL1-Parser/LL1-Parser.cpp
@@ -0,0 +1,105 @@
+#include "stdafx.h"
+//  Created by Patrick Shim on 10/8/15.
+//  Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+
+#include <iostream>
+#include <map>
+#include <stack>
+
+enum Symbols {
+	TS_L_PARENS,	// (
+	TS_R_PARENS,	// )
+	TS_A,		// a
+	TS_PLUS,	// +
+	TS_EOS,		// $, in this case corresponds to '\0'
+	TS_INVALID,	// invalid token
+
+				// Non-terminal symbols:
+	NTS_S,		// S
+	NTS_F		// F
+};
+
+enum Symbols lexer(char c)
+{
+	switch (c)
+	{
+	case '(':  return TS_L_PARENS;
+	case ')':  return TS_R_PARENS;
+	case 'a':  return TS_A;
+	case '+':  return TS_PLUS;
+	case '\0': return TS_EOS; // end of stack: the $ terminal symbol
+	default:   return TS_INVALID;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	using namespace std;
+
+	if (argc < 2)
+	{
+		cout << "usage:\n\tll '(a+a)'" << endl;
+		return 0;
+	}
+
+	// LL parser table, maps < non-terminal, terminal> pair to action	
+	map< enum Symbols, map<enum Symbols, int> > table;
+	stack<enum Symbols>	ss;	// symbol stack
+	char *p;	// input buffer
+
+				// initialize the symbols stack
+	ss.push(TS_EOS);	// terminal, $
+	ss.push(NTS_S);		// non-terminal, S
+
+						// initialize the symbol stream cursor
+	p = &argv[1][0];
+
+	// setup the parsing table
+	table[NTS_S][TS_L_PARENS] = 2;
+	table[NTS_S][TS_A] = 1;
+	table[NTS_F][TS_A] = 3;
+
+	while (ss.size() > 0)
+	{
+		if (lexer(*p) == ss.top())
+		{
+			cout << "Matched symbols: " << lexer(*p) << endl;
+			p++;
+			ss.pop();
+		}
+		else
+		{
+			cout << "Rule " << table[ss.top()][lexer(*p)] << endl;
+			switch (table[ss.top()][lexer(*p)])
+			{
+			case 1:	// 1. S ¡æ F
+				ss.pop();
+				ss.push(NTS_F);	// F
+				break;
+
+			case 2:	// 2. S ¡æ ( S + F )
+				ss.pop();
+				ss.push(TS_R_PARENS);	// )
+				ss.push(NTS_F);		// F
+				ss.push(TS_PLUS);	// +
+				ss.push(NTS_S);		// S
+				ss.push(TS_L_PARENS);	// (
+				break;
+
+			case 3:	// 3. F ¡æ a
+				ss.pop();
+				ss.push(TS_A);	// a
+				break;
+
+			default:
+				cout << "parsing table defaulted" << endl;
+				return 0;
+				break;
+			}
+		}
+	}
+
+	cout << "finished parsing" << endl;
+
+	return 0;
+}
---
+++ c++/LL1-Parser/LL1-Parser/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    CONSOLE APPLICATION : LL1-Parser Project Overview
+========================================================================
+
+AppWizard has created this LL1-Parser application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your LL1-Parser application.
+
+
+LL1-Parser.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+LL1-Parser.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+LL1-Parser.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named LL1-Parser.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
